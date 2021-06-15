38cee59eb899b2e4960d5e7a1b5b0beb7fbf7048
Sanju Kumari <63475754+SanjuWork@users.noreply.github.com>
2021-06-15 08:09:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+
+function preload(){
+
+}
+
+function setup() {
+ createCanvas(600,500);
+  
+
+}
+
+function draw() {
+ background("black");
+  
+ 
+} 
+  
+ 
+  drawSprites();
+  fill("lightpink");
+  textSize(20);
+   text("Score: "+ score, 500,50);
+}
+
